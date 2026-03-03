d1d947f940178753daf76ee191b9d336ed0c7634
Aymina Çakır <114817398+ayminacakir@users.noreply.github.com>
2026-03-03 17:57:08 +0300

    Initial Spring Boot project

---
+++ src/main/java/com/example/backendfal/BackendFalApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendfal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendFalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendFalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/backendfal/BackendFalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendfal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendFalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
